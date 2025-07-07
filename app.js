fb80a915322779f9154816ce19eeddf110b636b3
Joshua Ihwen <ihwenj@gmail.com>
2025-07-07 04:11:39 -0700

    updated file

---
+++ app.js
@@ -0,0 +1,47 @@
+// Variables and data types
+//data types (string)
+console.log("we are starting a great course")
+console.log("First Name " + "and " + "Final Name")
+console.log("ihwen." + "Josh" [0])
+console.log("ASAP Frontend" [12]);
+console.log("ASAP".length)
+//using variable and data types
+let string = "ASAP Frontend";
+console.log(string[string.length-3]);
+
+//data type (number)
+console.log(45%3);
+
+//data type (boolean)
+console.log(4>8);
+console.log(4<8);
+
+//data type (null and undefined)
+let Josh= null;
+console.log(Josh);
+
+//JS Variables (let)
+let FirstName = "Joshua";
+let LastName = "Ihwen";
+console.log("My name is " + FirstName + " " + LastName);
+console.log("My name is " + FirstName + " " + LastName);
+console.log("My name is " + FirstName + " " + LastName);
+console.log("My name is " + FirstName + " " + LastName);
+console.log("My name is " + FirstName + " " + LastName);
+
+//JS Variables (const)
+const userEmail = "ihwenj@gmail.com";
+console.log("my Email is " + userEmail);
+
+const userPassword = "Josh@1995";
+
+//Assignment
+let USD = 1200;
+let AUD= USD * 1.5;
+console.log("AUD to USD is " + AUD);
+
+// Comparison Operators
+console.log(5>=5);
+console.log(4<3);
+console.log("5"==5);
+console.log("5"===5);
